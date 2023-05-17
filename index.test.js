3d2cabbeb3cdee63d0f462ade0a5a8c6b347d614
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-17 22:29:27 +0100

    Third test - scissors vs. paper

--- index.test.js
+++ index.test.js
@@ -15,16 +15,34 @@
 
 //   expect(result).toBe(5);
 // });
-function rps(left) {
-  return left === "rock" ? "left" : "right";
+
+function rps(left, right) {
+  return left === "rock" || right === "paper" ? "left" : "right";
 }
 describe("rock, paper, scissors", () => {
-it("should say right wins for scissors vs. rock", () => {
-  const left = "scissors";
-  const right = "rock";
+  it("should say left wins for rock vs. scissors", () => {
+    const left = "rock";
+    const right = "scissors";
 
-  const result = rps(left, right);
+    const outcome = rps(left, right);
 
-  expect(result).toBe("right");
+    expect(outcome).toBe("left");
+  });
+  it("should say right wins for scissors vs. rock", () => {
+    const left = "scissors";
+    const right = "rock";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("right");
+  });
+  it("should say left wins for scissors vs. paper", () => {
+    const left = "scissors";
+    const right = "paper";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("left");
+  });
 });
-});
+
